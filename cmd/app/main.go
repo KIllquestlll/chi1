42dcad78e470287afd71a903bd98aa6038116c14
KIllquestlll <pubgmobailtestegora@gmail.com>
2026-02-01 05:48:08 +0600

    v1

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
+
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
